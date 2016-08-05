6d8e72cbc651a3aee653105503360b48c5604f09
Kyle Clark <kyleaclark@users.noreply.github.com>
2016-08-04 20:41:05 -0700

    Updated store per react starter kit feature_redux

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,37 +1,39 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
+import createHelpers from './createHelpers';
+import createLogger from './logger';
 
-const middleware = [thunk];
+export default function configureStore(initialState, helpersConfig) {
+  const helpers = createHelpers(helpersConfig);
+  const middleware = [thunk.withExtraArgument(helpers)];
 
-let enhancer;
+  let enhancer;
 
-if (__DEV__ && process.env.BROWSER) {
-  const createLogger = require('redux-logger');
-  const logger = createLogger({
-    collapsed: true,
-  });
-  middleware.push(logger);
-
-  enhancer = compose(
-    applyMiddleware(...middleware),
+  if (__DEV__) {
+    middleware.push(createLogger());
 
     // https://github.com/zalmoxisus/redux-devtools-extension#redux-devtools-extension
-    window.devToolsExtension ? window.devToolsExtension() : f => f,
-  );
-} else {
-  enhancer = applyMiddleware(...middleware);
-}
+    let devToolsExtension = f => f;
+    if (process.env.BROWSER && window.devToolsExtension) {
+      devToolsExtension = window.devToolsExtension();
+    }
 
-export default function configureStore(initialState) {
-  // Note: only Redux >= 3.1.0 supports passing enhancer as third argument.
+    enhancer = compose(
+      applyMiddleware(...middleware),
+      devToolsExtension,
+    );
+  } else {
+    enhancer = applyMiddleware(...middleware);
+  }
+
   // See https://github.com/rackt/redux/releases/tag/v3.1.0
   const store = createStore(rootReducer, initialState, enhancer);
 
   // Hot reload reducers (requires Webpack or Browserify HMR to be enabled)
   if (__DEV__ && module.hot) {
     module.hot.accept('../reducers', () =>
-      store.replaceReducer(require('../reducers').default)
+      store.replaceReducer(require('../reducers').default) // eslint-disable-line global-require
     );
   }
 
