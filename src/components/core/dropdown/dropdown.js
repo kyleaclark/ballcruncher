6e9fdc6663d4e1a856b077afb64dd1a35953c7f0
Kyle Clark <kyleaclark@users.noreply.github.com>
2017-06-01 14:21:29 -0700

    converted dropdown css

--- src/components/core/dropdown/dropdown.js
+++ src/components/core/dropdown/dropdown.js
@@ -2,8 +2,100 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import cx from 'classnames';
-import s from './dropdown.css';
+import styled from 'styled-components';
+
+const DropdownWrapper = styled.div`
+  display: inline-block;
+  margin-right: 5px;
+  position: relative;
+  width: 140px;
+`;
+
+const DropdownControl = styled.div`
+  position: relative;
+  overflow: hidden;
+  background-color: white;
+  border: 1px solid #ccc;
+  border-radius: 2px;
+  box-sizing: border-box;
+  color: #333;
+  cursor: default;
+  outline: none;
+  padding: 8px 52px 8px 10px;
+  transition: all 200ms ease;
+
+  &:hover {
+    box-shadow: 0 1px 0 rgba(0, 0, 0, 0.06);
+  }
+`;
+
+const DropdownMenu = styled.div`
+  background-color: white;
+  border: 1px solid #ccc;
+  box-shadow: 0 1px 0 rgba(0, 0, 0, 0.06);
+  box-sizing: border-box;
+  margin-top: -1px;
+  max-height: 200px;
+  overflow-y: scroll;
+  position: absolute;
+  top: 100%;
+  width: 100%;
+  z-index: 1000;
+  -webkit-overflow-scrolling: touch;
+`;
+
+const DropdownMenuGroupTitle = styled.div`
+  padding: 8px 10px;
+  color: rgba(51, 51, 51, 1.2);
+  font-weight: bold;
+  text-transform: capitalize;
+`;
+
+const DropdownOption = styled.div`
+  background-color: ${props => props.isSelected ? '#f2f9fc' : 'none'};
+  box-sizing: border-box;
+  color: ${props => props.isSelected ? '#333' : 'rgba(51, 51, 51, 0.8)'};
+  cursor: pointer;
+  display: block;
+  padding: 8px 10px;
+
+  &:last-child {
+    border-bottom-right-radius: 2px;
+     border-bottom-left-radius: 2px;
+  }
+
+  &:hover {
+    background-color: #f2f9fc;
+    color: #333;
+  }
+`;
+
+const DropdownArrow = styled.span`
+  border-color: #999 transparent transparent;
+  border-style: solid;
+  border-width: 5px 5px 0;
+  content: ' ';
+  display: block;
+  height: 0;
+  margin-top: -ceil(2.5);
+  position: absolute;
+  right: 10px;
+  top: 14px;
+  width: 0;
+`;
+
+const DropdownArrowOpen = styled(DropdownArrow)`
+  border-color: transparent transparent #999;
+  border-width: 0 5px 5px;
+`;
+
+const DropdownNoResults = styled.div`
+  box-sizing: border-box;
+  color: #ccc;
+  cursor: default;
+  display: block;
+  padding: 8px 10px;
+`;
 
 class Dropdown extends React.Component {
 
@@ -60,27 +152,27 @@ class Dropdown extends React.Component {
   }
 
   renderOption (option) {
-    let optionClass = cx({
-      [s.dropdown__option]: true,
-      [s.is_selected]: option == this.state.selected
-    });
+    const isSelected = option == this.state.selected;
 
     return (
-      <div key={option.value} className={optionClass} onMouseDown={this.setValue.bind(this, option)} onClick={this.setValue.bind(this, option)}>
+      <DropdownOption
+        key={option.value}
+        isSelected={isSelected}
+        onMouseDown={this.setValue.bind(this, option)}
+        onClick={this.setValue.bind(this, option)}>
         {option.label}
-      </div>
+      </DropdownOption>
     );
   }
 
   buildMenu() {
     let ops = this.props.options.map((option) => {
       if (option.type == 'group') {
-        let groupTitle = (<div className={s.title}>{option.name}</div>);
         let _options = option.items.map((item) => this.renderOption(item));
 
         return (
-          <div className={s.group} key={option.name}>
-            {groupTitle}
+          <div key={option.name}>
+            <DropdownMenuGroupTitle>{option.name}</DropdownMenuGroupTitle>
             {_options}
           </div>
         );
@@ -89,7 +181,7 @@ class Dropdown extends React.Component {
       }
     })
 
-    return ops.length ? ops : <div className={s.dropdown__noresults}>No options found</div>;
+    return ops.length ? ops : <DropdownNoResults>No options found</DropdownNoResults>;
   }
 
   handleDocumentClick(event) {
@@ -102,26 +194,30 @@ class Dropdown extends React.Component {
 
   render() {
     const { controlClassName, menuClassName } = this.props;
-    let menu = this.state.isOpen ? <div className={s[menuClassName]}>{this.buildMenu()}</div> : null;
+    let menuEl;
+    let dropdownEl;
 
-    let dropdownClass = cx({
-      [s.dropdown]: true,
-      [s.is_open]: this.state.isOpen
-    });
+    if (this.state.isOpen) {
+      menuEl = <DropdownMenu>{this.buildMenu()}</DropdownMenu>;
+      dropdownEl = <DropdownArrowOpen />;
+    } else {
+      menuEl = null;
+      dropdownEl = <DropdownArrow />;
+    }
 
     return (
-      <div className={dropdownClass}>
-        <div className={s[controlClassName]} onMouseDown={this.handleMouseDown.bind(this)} onTouchEnd={this.handleMouseDown.bind(this)}>
-          <div className={s.placeholder}>{this.state.selected.label}</div>
-          <span className={s.dropdown__arrow} />
-        </div>
-        {menu}
-      </div>
+      <DropdownWrapper>
+        <DropdownControl
+          onMouseDown={this.handleMouseDown.bind(this)}
+          onTouchEnd={this.handleMouseDown.bind(this)}>
+          <div>{this.state.selected.label}</div>
+          {dropdownEl}
+        </DropdownControl>
+        {menuEl}
+      </DropdownWrapper>
     );
   }
 
 }
-
-Dropdown.defaultProps = { controlClassName: 'dropdown__control', menuClassName: 'dropdown__menu'};
 
 export default Dropdown;
